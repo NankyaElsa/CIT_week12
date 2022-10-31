b84189c56ca7266430c7bf328f50bbdcc2ac6087
Kelly <elsakayigwa@gmail.com>
2022-10-31 18:40:15 +0000

    quiz

--- quiz.py
+++ quiz.py
@@ -30,25 +30,32 @@
 
 #Question 4
 #create a 6x6 numpy array with random values and find the min and max values
+# import numpy as np
+# numbers=np.array([
+#     [4, 1, -7, 0, 14, 64],
+#     [63, 72, -6, 32, -100, 3],
+#     [11, 34, 653, 84, -500, 1],
+#     [12, 9, 24, 18, 7, 90],
+#     [31, 84, 76, 91, 40, 20],
+#     [51, 63, 70, -20, 5, 47]
+# ])
+# min=np.min(numbers)
+# max=np.max(numbers)
+# print(f"minimum value is {min} and maximum value is {max}")
+
+
+#Question 5
+#create a 3D numpy array and reshape it to 2D
 import numpy as np
-numbers=np.array([
-    [4, 1, -7, 0, 14, 64],
-    [63, 72, -6, 32, -100, 3],
-    [11, 34, 653, 84, -500, 1],
-    [12, 9, 24, 18, 7, 90],
-    [31, 84, 76, 91, 40, 20],
-    [51, 63, 70, -20, 5, 47]
+_3d=np.array([
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12]
 ])
-min=np.min(numbers)
-max=np.max(numbers)
-print(f"minimum value is {min} and maximum value is {max}")
+_2d=np.reshape(_3d, [2, 6])
+print(_2d)
 
-
-
-"""Question 5
-create a 3D numpy array and reshape it to 2D
-
-Question 6
+"""Question 6
 create 1D array from this data
 
     data = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
